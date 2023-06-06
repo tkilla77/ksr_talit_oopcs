88e164b22184154ceab4f5f864ea3aceea3106ac
Tom Hofmann <tom@scheidweg.net>
2023-06-06 23:41:21 +0200

    change Main to start Monogame

--- Program.cs
+++ Program.cs
@@ -1,16 +1,3 @@
-﻿using geometry;
-
-using (Canvas canvas = new Canvas()) {
-//    canvas.Line(new Vector(100,100), new Vector(200,200));
-
-    Figure triangle = new Polygon(new Vector(100, 100), new Vector(100, 200), new Vector(200, 100));
-    triangle.Draw(canvas);
-
-    Figure square = new Square(new Vector(300,300), new Vector(100, 50));
-    square.Draw(canvas);
-
-    Figure circle = new Circle(new Vector(400, 100), new Vector(100,0));
-    circle.Draw(canvas);
-
-    canvas.Save("example.png");
-}
+﻿
+using var game = new surface.Surface();
+game.Run();
