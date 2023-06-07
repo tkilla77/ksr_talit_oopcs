36c08a3c911b5301055e40cdb2a15e37ff420c68
Tom Hofmann <tom@scheidweg.net>
2023-06-07 15:28:34 +0200

    better scene

--- CircleScene.cs
+++ CircleScene.cs
@@ -8,6 +8,7 @@ public class CircleScene : Scene {
         circle = new Circle(new Vector(50, 240), new Vector(30, 0));
         circle.SetColors("000", "af00");
         figures.Add(circle);
+        figures.Add(new Square(new Vector(300, 300), new Vector(50, 0)));
     }
 
     public override void Update(double totalSeconds, double elapsedSeconds) {
